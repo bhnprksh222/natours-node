81f1095318e32d53feb35fa245a9388b326cfaef
bhnprksh222 <bhnprksh222@gmail.com>
2020-05-12 12:03:52 +0530

    chaining multiple middlewares!

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ const app = express();
 //! 1) Middlewares
 app.use(morgan('dev'));
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware😊');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware😊');
+//   next();
+// });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
 
 //! 3) ROUTES
 
