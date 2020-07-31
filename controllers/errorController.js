9b4d9d4ff4cc4f27329ebdca4eaf02a8acba72e2
Bhanu Prakash <bhnprksh222@gmail.com>
2020-07-31 12:55:25 +0530

    Handling Invalid Database🗄️ IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -15,16 +22,15 @@ const sendErrorProd = (err, res) => {
       message: err.message,
     });
   }
-  // Programming or the other unknown error: don't leak error details 
+  // Programming or the other unknown error: don't leak error details
   else {
     // 1) Log error
     console.error('ERROR❌', err);
-
     // 2) Send generation message
     res.status(500).json({
       status: 'error',
       message: 'Something went wrong',
-    })
+    });
   }
 };
 
@@ -34,9 +40,14 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if(process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    // let error = { ...err };
+    let error = Object.assign(err);
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 }
